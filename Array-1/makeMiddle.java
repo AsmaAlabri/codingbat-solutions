c690fd57c9a7cc9c01d6c14769bdbd3de63fdc1f
AsmaAlabri <aa8454854@gmail.com>
2025-08-02 13:12:42 +0400

    Add files via upload

---
+++ Array-1/makeMiddle.java
@@ -0,0 +1,6 @@
+public class makeMiddle {
+    public int[] makeMiddle(int[] nums) {
+        int middle = nums.length / 2;
+        return new int[] {nums[middle - 1], nums[middle]};
+    }
+}
---
+++ Array-1/midThree.java
@@ -0,0 +1,7 @@
+public class midThree {
+    public int[] midThree(int[] nums) {
+        int middle = nums.length / 2;
+        return new int[] {nums[middle - 1], nums[middle], nums[middle + 1]};
+    }
+
+}
